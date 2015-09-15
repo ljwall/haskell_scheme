ecbe455325e41020d48d14e8466f50eb658305fe
Liam Wall <liam.wall@gmail.com>
2015-09-15 20:47:36 +0100

    Use System.Console.Haskeline better REPL UX

--- src/main.hs
+++ src/main.hs
@@ -3,7 +3,8 @@ import Control.Monad.Except (catchError, throwError)
 import HSchemeParse
 import HSchemeEval
 import LispError
-import System.IO (hFlush, stdout)
+import System.Console.Haskeline
+  (runInputT, InputT, defaultSettings, getInputLine, outputStrLn)
 
 readExpr :: String -> ThrowsLispError LispVal
 readExpr expression =
@@ -11,28 +12,19 @@ readExpr expression =
     Left err -> throwError . Parser $ err
     Right val -> return val
 
-readPrompt :: String -> IO String
-readPrompt prompt = putStr prompt >> hFlush stdout >> getLine
-
-continue :: String -> Bool
-continue "quit" = False
-continue "q" = False
-continue _ = True
-
-processInput :: String -> IO ()
-processInput expr = putStrLn $ extractValue result
+processInput :: String -> InputT IO ()
+processInput expr = outputStrLn $ extractValue result
   where result = ((readExpr expr) >>= eval >>= (return . show))
                     `catchError` (\err -> (return . show $ err))
 
-
-until_ :: (Monad m) => (a -> Bool) -> m a -> (a -> m ()) -> m ()
-until_ predicate prompt action = do
-  expr <- prompt
-  if predicate expr
-    then action expr >> until_ predicate prompt action
-    else return ()
-
 main :: IO ()
-main = do
-  putStrLn "Welcome to hlisp. 'q' or 'quit' to quit.\n"
-  until_ continue (readPrompt "hlisp>> ") processInput
+main = runInputT defaultSettings loop
+  where
+    loop :: InputT IO ()
+    loop = do
+      minput <- getInputLine "hlisp>> "
+      case minput of
+        Nothing -> return ()
+        Just "quit" -> return ()
+        Just "q" -> return ()
+        Just expr -> processInput expr >> loop
