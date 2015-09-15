c93250ff80d18a3e09c0b67e09523942d464fa68
Liam Wall <liam.wall@gmail.com>
2015-09-15 20:12:40 +0100

    Use a REPL rather than cmd line input

--- src/main.hs
+++ src/main.hs
@@ -1,9 +1,9 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
-import System.Environment
 import Control.Monad.Except
 import HSchemeParse
 import HSchemeEval
 import LispError
+import System.IO (hFlush, stdout)
 
 readExpr :: String -> ThrowsLispError LispVal
 readExpr expression =
@@ -11,9 +11,28 @@ readExpr expression =
     Left err -> throwError . Parser $ err
     Right val -> return val
 
+readPrompt :: String -> IO String
+readPrompt prompt = putStr prompt >> hFlush stdout >> getLine
+
+continue :: String -> Bool
+continue "quit" = False
+continue "q" = False
+continue _ = True
+
+processInput :: String -> IO ()
+processInput expr = putStrLn $ extractValue result
+  where result = ((readExpr expr) >>= eval >>= (return . show))
+                    `catchError` (\err -> (return . show $ err))
+
+
+until_ :: (Monad m) => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ predicate prompt action = do
+  expr <- prompt
+  if predicate expr
+    then action expr >> until_ predicate prompt action
+    else return ()
+
 main :: IO ()
 main = do
-  fst_arg:other_args <- getArgs
-  let result = ((readExpr fst_arg) >>= eval >>= (return . show))
-                  `catchError` (\err -> (return . show $ err))
-  putStrLn $ extractValue result
+  putStrLn "Welcome to hlisp. 'q' or 'quit' to quit.\n"
+  until_ continue (readPrompt "hlisp>> ") processInput
